228ce75a48d7f9135ac1121a1bd85d13648da630
TA3008 <nguyentuananh30082002@gmail.com>
2025-06-30 17:27:30 +0700

    add: apply phpredis and install bunjs

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/test-redis', function () {
+    Redis::set('demo_key', 'hello redis');
+    return Redis::get('demo_key');
 });
